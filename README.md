d8deb54cac37ae74769ceba04b29d8af5f4371fd
sh4869 <nobuk4869@gmail.com>
2016-11-13 21:53:45 +0900

    Init Data And Scripts

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # miteta-web
 
 My Browsing Website from Twitter #miteru Tweet
+
+## Tweet-data Branch
+
+This branch provide tweet data json file.
---
+++ script/init_from_csv.rb
@@ -0,0 +1,29 @@
+require 'open-uri'
+require 'csv'
+require 'json'
+
+miteru_datas = []
+CSV.foreach("tweets.csv",encoding:"UTF-8:UTF-8") do |tweet|
+  text = tweet[5]
+  if !text.to_s.start_with?("RT") && text.to_s.include?("#miteru")
+    page_url = tweet[9].split(",").last
+    begin 
+      title = OpenURI.open_uri(page_url).read.scan(/<title>(.*?)<\/title>/)[0][0].force_encoding("UTF-8")
+    rescue  => e
+      title = ""
+    end
+    tweet_url = "https://twitter.com/statues/" + tweet[0].to_s
+    tweet_data = {"url" => page_url,"title" => title, "tweet_url" => tweet_url}
+    begin 
+      puts tweet_data.to_json
+      miteru_datas.push(tweet_data)
+    rescue => e
+      puts e
+    end
+  end
+end
+
+puts miteru_datas.to_json
+open("miteru_tweet.json","w") do |io|
+  JSON.dump(miteru_datas.to_json,io)
+end
