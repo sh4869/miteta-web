886a352eb69ee5f9b89dee759a8029fad20e658e
sh4869 <nobuk4869@gmail.com>
2018-01-23 18:10:03 +0900

    [ci skip] AUTOMATIC UPDATE

--- script/updater.rb
+++ script/updater.rb
@@ -4,11 +4,9 @@ require_relative 'git_updater.rb'
 
 Forever.run do
   every 5.minutes do 
-    puts "start"
     increments_tweet = MiteruTweetChecker.check
     if increments_tweet
       GitUpdater.update_repository
     end
-    puts "Finish"
   end
 end
